a11ce6031a4b9579ba8e2b4edb12078b2889a7cf
ashipovalov <ashipovalov@gmail.com>
2018-09-05 10:28:08 +0300

    Финальные правки по третьему занятию

--- src/test/java/shouty/RunCukesTest.java
+++ src/test/java/shouty/RunCukesTest.java
@@ -1,10 +1,13 @@
 package shouty;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "io.cucumber.pro.JsonReporter:all"})
+@CucumberOptions(features = {"src\\test\\resources\\shouty\\"},
+snippets = SnippetType.CAMELCASE)
+
 public class RunCukesTest {
 }
