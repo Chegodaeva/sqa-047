b99b0dea34e4e6bdba3fe52fcd697a8af8eea6f9
ashipovalov <ashipovalov@gmail.com>
2018-09-05 10:17:15 +0300

    Добавлен класс Stepdefs

---
+++ src/test/java/shouty/Stepdefs.java
@@ -0,0 +1,30 @@
+package shouty;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Stepdefs {
+
+    @Given("Lucy is (\\d+)m from Sean")
+    public void lucy_is_m_from_Sean(int arg1) {
+        System.out.println(arg1);
+
+    }
+
+    @When("Sean shouts \"(.*?)\"$")
+    public void sean_shouts(String arg1) {
+        System.out.println(arg1);
+
+    }
+
+    @Then("Lucy should hear Sean's message")
+    public void lucy_should_hear_Sean_s_message() {
+        System.out.println("Close step");
+
+    }
+
+
+
+}
