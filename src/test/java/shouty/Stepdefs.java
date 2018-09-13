c4bcaeed909b4d691533c64728c8476c4b6f190b
ashipovalov <ashipovalov@gmail.com>
2018-09-13 09:48:39 +0300

    Добавлен пример работы с Actions

--- src/test/java/shouty/Stepdefs.java
+++ src/test/java/shouty/Stepdefs.java
@@ -2,15 +2,23 @@ package shouty;
 
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+
+import org.junit.Assert;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+
 import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+
 
 import java.util.concurrent.TimeUnit;
 
@@ -31,20 +39,49 @@ public class Stepdefs {
     @When("I fill login form")
     public void iFillLoginForm(){
         driver.findElement(By.cssSelector("form[name='login_form']"))
-                .findElement(By.cssSelector("input[name='username']"));
+                .findElement(By.cssSelector("input[name='username']")).clear();
+        driver.findElement(By.cssSelector("input[name='username']")).sendKeys("student");
 
 
-        driver.findElement(By.cssSelector("input[name='password']"));
+        driver.findElement(By.cssSelector("input[name='password']")).clear();
+        driver.findElement(By.cssSelector("input[name='password']")).sendKeys("luxoft");
 
 
-        driver.findElement(By.cssSelector("input.button"));
+        driver.findElement(By.cssSelector("input.button")).click();
         WebDriverWait wait =new WebDriverWait(driver, 5, 1000);
 
         wait.until(elementToBeClickable(By.linkText("Logout")));
 
 
-        driver.quit();
+
+
+
+
+
     }
+
+    @Then("Logout link is visible")
+    public void logoutLinkIsVisible() {
+        Assert.assertTrue(driver.findElement(By.linkText("Logout")).isDisplayed());
+    }
+
+    @Then("Select Project is visible")
+    public void selectProjectIsVisible() throws InterruptedException {
+        Select selectProject = new Select(driver.findElement(By.cssSelector("select[name='project_id']")));
+        selectProject.selectByVisibleText("11122");
+        Thread.sleep(5000);
+
+        //Пример работы с Actions
+        new Actions(driver)
+                .moveToElement(driver.findElement(By.cssSelector("img[alt='Powered by Mantis Bugtracker']")))
+                .contextClick()
+                .perform();
+
+
+        driver.quit();
+
+    }
+
 
 
 }
--- src/test/resources/mantis/login.feature
+++ src/test/resources/mantis/login.feature
@@ -4,6 +4,5 @@ Feature: Login
   Scenario: User can login with right credentianls
     Given I open login page
     When I fill login form
-
-
-
+    Then Logout link is visible
+    And Select Project is visible
